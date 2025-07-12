b0e40c968ff37cbbfbb0b2132fda55775678c411
Yoann Joseph <yoannjoseph16062000@gmail.com>
2025-07-12 04:38:57 -0300

    refactor: learning python
    I learned how to move a file in python

--- file_watcher.py
+++ file_watcher.py
@@ -9,10 +9,17 @@ class EventManager(FileSystemEventHandler):
         if not event.is_directory:
             print(f"File created: {event.src_path}")
             if event.src_path.endswith('.h'):
+                self.move_file(event.src_path, "Game/include/")
                 print(f" Is a header file")
             elif event.src_path.endswith('.cpp'):
+                self.move_file(event.src_path, "Game/src/")
                 print(f"Is a source file")
             else:
                 print(f"Ignoring file: {event.src_path}")
+    def move_file(self,source,destination):
+        time.sleep(3)
+        name_file = os.path.basename(source)
+        destination_path = os.path.join(destination, name_file)
+        shutil.move(source, destination_path)
 
 

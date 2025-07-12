b6fc8fbf85c76c5e6d88e15f3032925a7c53efe9
Yoann Joseph <yoannjoseph16062000@gmail.com>
2025-07-12 16:10:20 -0300

    feat: learning python
    I added an instance of my EventManager and learned how to monitor a folder with Observer

--- file_watcher.py
+++ file_watcher.py
@@ -10,10 +10,8 @@ class EventManager(FileSystemEventHandler):
             print(f"File created: {event.src_path}")
             if event.src_path.endswith('.h'):
                 self.move_file(event.src_path, "Game/include/")
-                print(f" Is a header file")
             elif event.src_path.endswith('.cpp'):
                 self.move_file(event.src_path, "Game/src/")
-                print(f"Is a source file")
             else:
                 print(f"Ignoring file: {event.src_path}")
     def move_file(self,source,destination):
@@ -21,5 +19,23 @@ class EventManager(FileSystemEventHandler):
         name_file = os.path.basename(source)
         destination_path = os.path.join(destination, name_file)
         shutil.move(source, destination_path)
+        print(f"Moved file : {name_file} to {destination_path}")
+
+
+print("=== Starting file watcher ===")
+print("To stop press Ctrl+C")
+
+manager = EventManager()
+
+observer = Observer()
+observer.schedule(manager,"build/Game/", recursive=False)
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+    print("=== Stopping file watcher ===")
+observer.join()
 
 
